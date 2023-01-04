32f5dc2098a56ca60b0391d8a1c16edeee53dc51
dermo <dermot.logue@dkit.ie>
2023-01-04 17:59:49 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+// sam101
+
+#include <iostream> // Input-Output Stream library
+
+// function declaration (or function prototype)
+int add(int, int);  // a function must be declared before it is called.
+
+
+int main() {
+    std::cout << "sam101 - calling add function from main()" << std::endl;
+    int sum = add(2,2);
+    std::cout <<  "Sum of values = " << sum << std::endl;
+    return 0;
+}
+
+// function definition
+int add( int x, int y) {    // two int parameters, returns int
+    return x + y;
+}
+
+//TODO
+// 1. Define a function multiply(int,int) that takes two integer
+// arguments and returns an int value.  Test your function
+// and output the value it returns.
+
+//TODO
+// 2. Define a function divide(int,int) that takes two integer arguments
+// and returns the result of dividing the first by the second.
+// Consider what the return type should be.
+// Output your result. Try calling divide(3,2)
+
